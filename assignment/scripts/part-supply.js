e606e235f2a688cc1ddece876be36630a502f4ac
Curtis Kettler <curtiskettler@gmail.com>
2021-06-22 09:06:55 -0500

    while loop complete

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -45,7 +45,17 @@ console.log('7. Showing supplyChanges with "for of" loop');
 
 // 8. Rewrite the `for` loop from #6 as a `while` loop.
 console.log('8. Showing supplyChanges with "while" loop');
-
+let index = 0;
+while (index < supplyChanges.length){
+  if (supplyChanges[index] > 0){
+    console.log(`Added ${supplyChanges[index]} parts.`);
+  } else if (supplyChanges[index] === 0){
+    console.log('No change.');
+  } else if (supplyChanges[index] < 0){
+    console.log(`Removed ${supplyChanges[index]} parts.`);
+  }
+  index++;
+}
 
 // 9. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
