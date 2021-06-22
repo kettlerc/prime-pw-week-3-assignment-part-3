707af4b11721059ce6d2fb45f828173c533a8da0
Curtis Kettler <curtiskettler@gmail.com>
2021-06-22 09:02:27 -0500

    so excited I got #6 on the first try so I had to commit

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -28,7 +28,15 @@ console.log(supplyChanges);
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.'
 console.log('6. Showing supplyChanges...');
-
+for (let i=0; i<supplyChanges.length; i++){
+  if (supplyChanges[i] > 0){
+    console.log(`Added ${supplyChanges[i]} parts.`);
+  } else if (supplyChanges[i] === 0){
+    console.log('No change.');
+  } else if (supplyChanges[i] < 0){
+    console.log(`Removed ${supplyChanges[i]} parts.`);
+  }
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
